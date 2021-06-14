f638ef0f8e0c0af6bd4ef65f75ecadff757bd5bb
mike.bskim <mike.bskim@gmail.com>
2021-06-14 16:40:27 +0200

    update - navigation

---
+++ lib/src/app.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar test'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0,
+        showSelectedLabels: true,
+        selectedItemColor: Colors.black,
+        onTap: (index) {
+            print(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/svg/icons/home_off.svg'),
+            activeIcon: SvgPicture.asset('assets/svg/icons/home_on.svg'),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/svg/icons/compass_off.svg',
+              width: 22,
+            ),
+            activeIcon: SvgPicture.asset(
+              'assets/svg/icons/compass_on.svg',
+              width: 22,
+            ),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: SvgPicture.asset(
+                'assets/svg/icons/plus.svg',
+                width: 35,
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/svg/icons/subs_off.svg'),
+            activeIcon: SvgPicture.asset('assets/svg/icons/subs_on.svg'),
+            label: 'Subscribe',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/svg/icons/library_off.svg'),
+            activeIcon: SvgPicture.asset('assets/svg/icons/library_on.svg'),
+            label: 'Favorite',
+          ),
+        ],
+      ),
+    );
+  }
+}
