e04bfe425863f5a72186921e5be49cdde13d5382
mike.bskim <mike.bskim@gmail.com>
2021-06-18 16:01:53 +0200

    youtube #12 done

--- lib/src/repository/youtube_repository.dart
+++ lib/src/repository/youtube_repository.dart
@@ -16,7 +16,7 @@ class YoutubeRepository extends GetConnect {
   Future<YoutubeVideoResult?> loadVideos(String nextPageToken) async {
     // 15:30초 // Future<YoutubeVideoResult>
     String url =
-        '/youtube/v3/search?part=snippet&channelId=UCbMGBIayK26L4VaFrs5jyBw&maxResults=10&order=date&type=video&videoDefinition=high&key=본인키사용&pageToken=$nextPageToken';
+        '/youtube/v3/search?part=snippet&channelId=UCbMGBIayK26L4VaFrs5jyBw&maxResults=10&order=date&type=video&videoDefinition=high&key=AIzaSyAi2Rq_uTmtvsbmdJaOdkJlAB7dlQjvv9k&pageToken=$nextPageToken';
     final resp = await get(url);
 
     if (resp.status.hasError) {
@@ -30,7 +30,7 @@ class YoutubeRepository extends GetConnect {
 
   Future<Statistics?> getVideoInfoById(String videoId) async {
     String url =
-        '/youtube/v3/videos?part=statistics&key=본인키사용&id=$videoId';
+        '/youtube/v3/videos?part=statistics&key=AIzaSyAi2Rq_uTmtvsbmdJaOdkJlAB7dlQjvv9k&id=$videoId';
     final resp = await get(url);
 
     if (resp.status.hasError) {
@@ -45,7 +45,7 @@ class YoutubeRepository extends GetConnect {
 
   Future<Youtuber?> getYoutuberInfoById(String channelId) async {
     String url =
-        '/youtube/v3/channels?part=statistics,snippet&key=본인키사용&id=$channelId';
+        '/youtube/v3/channels?part=statistics,snippet&key=AIzaSyAi2Rq_uTmtvsbmdJaOdkJlAB7dlQjvv9k&id=$channelId';
     final resp = await get(url);
 
     if (resp.status.hasError) {
